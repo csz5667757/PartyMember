7eb52970c07a5efe63aed7f6ff97e9ba3bd32386
csz <956765145@qq.com>
2019-06-24 22:09:33 +0800

    初始化项目

---
+++ src/main/java/com/assertor/partyMember/PartyMemberApplication.java
@@ -0,0 +1,13 @@
+package com.assertor.partyMember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyMemberApplication.class, args);
+	}
+
+}
